06dcb4426335ac825f5523bcefb4b61f7c3690af
jonathansa-98 <daw2jsuarezalvarez@iesjoanramis.org>
2019-01-20 18:18:31 +0100

    Card match checked

--- js/app.js
+++ js/app.js
@@ -1,13 +1,38 @@
+/* Card class*/
 function Card(id, img) {
-    this.id = id || -1;
+    this.id = id;
     this.img = img || "";
 }
+/*************************************/
+
+var cards = document.querySelectorAll('.card');
+var moves = document.querySelector('.moves');
+var cards_matched = [];
+var all_cards = [];
+
+cards.forEach(card => card.addEventListener('click', function(e) {
+    openCard(card);
+    cards_matched.push(card);
+    if (cards_matched.length % 2 == 0) {
+        var card1 = cards_matched.slice(-2, -1);
+        var card2 = cards_matched.slice(-1);
+        card1 = card1[0];
+        card2 = card2[0];
+        if (cardsMatch(card1, card2)) {
+            console.log("bien");
+        } else {
+            console.log("mal");
+        }
+    }
+}));
 
 function startGame() {
-    suffleDeck();
+    all_cards = [];
+    suffleDeck(all_cards);
+    console.log(all_cards);
 }
 
-/* Returns an array sorted randomly with all the icons to put 
+/* Returns an array sorted randomly with all the icons to put
 in the <i> classnames*/
 function getImages() {
     var images = ["fab fa-apple", "fab fa-android", "fas fa-baby",
@@ -18,14 +43,25 @@ function getImages() {
     return images;
 }
 
-function suffleDeck() {
-    var cards = document.querySelectorAll('.card');
-    var img;
+function suffleDeck(all_cards) {
+    var i_tag;
     var images = getImages();
     for (var i = 0; i < cards.length; i++) {
-        img = cards[i].getElementsByTagName('i')[0];
-        img.className = images[i];
+        i_tag = cards[i].getElementsByTagName('i')[0];
+        i_tag.className = images[i];
+        // create card objects
+        all_cards.push(new Card(i, images[i]));
     }
+}
+
+function openCard(card) {
+    card.className = "card open show disabled";
+}
+
+function cardsMatch(card1, card2) {
+    var id_card1 = parseInt(card1.getAttribute('id'));
+    var id_card2 = parseInt(card2.getAttribute('id'));
+    return all_cards[id_card1].img === all_cards[id_card2].img;
 }
 
 /* Executing*/
