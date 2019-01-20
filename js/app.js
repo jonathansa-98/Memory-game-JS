f490c95bc9ea6b8b297f205454c65e22dde57988
jonathansa-98 <daw2jsuarezalvarez@iesjoanramis.org>
2019-01-20 16:36:00 +0100

    Shuffle done.

--- js/app.js
+++ js/app.js
@@ -7,11 +7,26 @@ function startGame() {
     suffleDeck();
 }
 
+/* Returns an array sorted randomly with all the icons to put 
+in the <i> classnames*/
+function getImages() {
+    var images = ["fab fa-apple", "fab fa-android", "fas fa-baby",
+                  "fas fa-bug", "fas fa-cannabis", "fas fa-american-sign-language-interpreting",
+                  "fab fa-apple", "fab fa-android", "fas fa-baby",
+                  "fas fa-bug", "fas fa-cannabis", "fas fa-american-sign-language-interpreting"];
+    images.sort(function(){return 0.5 - Math.random();});
+    return images;
+}
+
 function suffleDeck() {
-    cards = document.querySelectorAll('.card');
-    for (var card of cards) {
-        console.log(card);
+    var cards = document.querySelectorAll('.card');
+    var img;
+    var images = getImages();
+    for (var i = 0; i < cards.length; i++) {
+        img = cards[i].getElementsByTagName('i')[0];
+        img.className = images[i];
     }
 }
 
+/* Executing*/
 startGame();
