a0ef388477c049ef9f78b8fffc4bc6e78a230ade
jonathansa-98 <daw2jsuarezalvarez@iesjoanramis.org>
2019-01-20 19:13:48 +0100

    Game ends nice :D

--- js/app.js
+++ js/app.js
@@ -19,22 +19,30 @@ cards.forEach(card => card.addEventListener('click', function(e) {
         card1 = card1[0];
         card2 = card2[0];
         if (cardsMatch(card1, card2)) {
-            console.log("bien");
             console.log(cards_matched);
             higlightMatchedCards(card1, card2);
+            if (cards_matched.length == all_cards.length) {
+                alert("Has ganado!!");
+            }
         } else {
-            console.log("mal");
             cards_matched.splice(-2, 2);
             console.log(cards_matched);
+            higlightUnmatchedCards(card1, card2);
         }
         moves.innerHTML = parseInt(moves.innerHTML)+1;
     }
 }));
 
 function startGame() {
-    all_cards = [];
+    restart();
     suffleDeck(all_cards);
     console.log(all_cards);
+}
+
+function restart() {
+    all_cards = [];
+    cards_matched = [];
+    moves.innerHTML = 0;
 }
 
 /* Returns an array sorted randomly with all the icons to put
@@ -60,12 +68,44 @@ function suffleDeck(all_cards) {
 }
 
 function openCard(card) {
-    card.className = "card open show disabled";
+    card.classList.add("open", "show", "disabled");
 }
 
 function higlightMatchedCards(card1, card2) {
-    card1.className = "card match show disabled";
-    card2.className = "card match show disabled";
+    card1.classList.add("match");
+    card2.classList.add("match");
+    card1.classList.remove("open");
+    card2.classList.remove("open");
+}
+
+function higlightUnmatchedCards(card1, card2){
+    card1.classList.add("unmatched");
+    card2.classList.add("unmatched");
+    card1.classList.remove("open");
+    card2.classList.remove("open");
+    disable();
+    setTimeout(function(){
+        card1.classList.remove("show", "open", "unmatched");
+        card2.classList.remove("show", "open", "unmatched");
+        enable();
+    },750);
+}
+
+// disable cards temporarily
+function disable(){
+    Array.prototype.filter.call(cards, function(card){
+        card.classList.add('disabled');
+    });
+}
+
+// enable cards and disable matched cards
+function enable(){
+    Array.prototype.filter.call(cards, function(card){
+        card.classList.remove('disabled');
+        for(var i = 0; i < cards_matched.length; i++){
+            cards_matched[i].classList.add("disabled");
+        }
+    });
 }
 
 function cardsMatch(card1, card2) {
